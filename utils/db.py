d7d6a8a8bc0640d38b660d3ba29e2147fa3cf3fc
P-Asare <pinopalal11@gmail.com>
2024-04-14 14:12:15 +0100

    connection changes

--- utils/db.py
+++ utils/db.py
@@ -1,11 +1,31 @@
-import mysql.connector
+# import mysql.connector
 
-db_config = {
-    'host': '16.171.235.91',
-    'user': 'root',
-    'password': '5cdswaakye',
-    'database': 'scholarsphere'
-}
+# db_config = {
+#     'host': '16.171.235.91',
+#     'user': 'root',
+#     'password': '5cdswaakye',
+#     'database': 'scholarsphere'
+# }
+
+# def get_db_connection():
+#     return mysql.connector.connect(**db_config)
+
+import pyodbc
+
+# Azure SQL database connection configuration
+server = 'final-valid.database.windows.net'
+database = 'scholarsphere'
+username = 'rootpalal'
+password = '5cdsW@@kye'
+driver = '{ODBC Driver 17 for SQL Server}'  # Make sure to install this driver
 
 def get_db_connection():
-    return mysql.connector.connect(**db_config)
+    try:
+        conn = pyodbc.connect(
+            f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+        )
+        print("Connection established successfully")
+        return conn
+    except Exception as e:
+        print(f"Error connecting to the database: {str(e)}")
+        return None
